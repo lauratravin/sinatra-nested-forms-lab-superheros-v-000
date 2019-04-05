be42579f243c1e3a22c7c8b62db57201ae40e342
Laura Messa Travin <laura.travin@gmail.com>
2019-04-05 00:12:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
 
     post '/new' do
        binding.pry
-    @team=   Team.new(name: params[:team][:name],motto: params[:team][:motto])
+    @team=  Team.new(name: params[:team][:name],motto: params[:team][:motto])
 
       params[:team][:members].each do |m|
         Hero.new(m)
